8d386e78986a15bc5b19ed9dc81e4e497b3d4ded
steve-conrad <steve.conrad14@gmail.com>
2025-07-21 12:57:24 -0400

    added wofi power button

--- fabric/bar.py
+++ fabric/bar.py
@@ -1,11 +1,16 @@
 import fabric
+import subprocess
+import os
 from fabric import Application
 from fabric.widgets.datetime import DateTime
 from fabric.widgets.centerbox import CenterBox
 from fabric.widgets.box import Box
 from fabric.widgets.button import Button
 from fabric.widgets.wayland import WaylandWindow as Window
+from fabric.widgets.label import Label
 from fabric.utils import get_relative_path, monitor_file
+from fabric.audio import Audio
+from gi.repository import Gdk
 from modules.power_menu import PowerMenu
 from fabric.hyprland.widgets import (
         Language,
@@ -25,14 +30,17 @@ class Bar(Window):
             **kwargs
         )
 
-        self.set_size_request(1400, 30)
+        default_bar_size = (1400, 30)
+        self.set_size_request(*default_bar_size)
 
         self.power_button = Button (
             label=" ",
-            name="power-button",
+            name="power-button"
         )
+        self.power_button.connect("clicked", self.run_power_menu_script)
+
         self.date_time = DateTime(
-            name="date-time",
+            name="date-time"
         )
         self.workspaces = Workspaces(
             name="workspaces"
@@ -40,13 +48,20 @@ class Bar(Window):
         self.active_window = ActiveWindow(
             name="active-window"
         )
+        self.audio_button = Audio(
+        )
+
         self.children = CenterBox(
             start_children=[self.power_button, self.date_time],
             center_children=[self.workspaces],
             end_children=[self.active_window],
+        )   
+
+    def run_power_menu_script(self, *args):
+        script_path = os.path.expandvars(
+          "$HOME/.config/hyprnosis/configs/waybar/scripts/power_menu.sh"
         )
-
-
+        subprocess.Popen([script_path])
 
 if __name__ == "__main__":
     bar = Bar()
