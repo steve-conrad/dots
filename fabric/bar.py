ea452253e2a81847f7cb3a22d53e5b7a184651c9
steve-conrad <steve.conrad14@gmail.com>
2025-07-22 13:25:45 -0400

    added bluetooth and network buttons and functionality

--- fabric/bar.py
+++ fabric/bar.py
@@ -4,14 +4,9 @@ import os
 from fabric import Application
 from fabric.widgets.datetime import DateTime
 from fabric.widgets.centerbox import CenterBox
-from fabric.widgets.box import Box
 from fabric.widgets.button import Button
 from fabric.widgets.wayland import WaylandWindow as Window
-from fabric.widgets.label import Label
 from fabric.utils import get_relative_path, monitor_file
-from fabric.audio import Audio
-from gi.repository import Gdk
-from modules.power_menu import PowerMenu
 from fabric.hyprland.widgets import (
         Language,
         WorkspaceButton,
@@ -30,8 +25,8 @@ class Bar(Window):
             **kwargs
         )
 
-        default_bar_size = (1400, 30)
-        self.set_size_request(*default_bar_size)
+        self.default_bar_size = (1400, 30)
+        self.set_size_request(*self.default_bar_size)
 
         self.power_button = Button (
             label=" ",
@@ -48,13 +43,22 @@ class Bar(Window):
         self.active_window = ActiveWindow(
             name="active-window"
         )
-        self.audio_button = Audio(
+        self.bluetooth = Button(
+            label=" ",
+            name="bluetooth-button"
         )
+        self.bluetooth.connect("clicked", self.run_bluetooth_script)
+
+        self.network = Button(
+            label=" ",
+            name="network-button"
+        )
+        self.network.connect("clicked", lambda *args: subprocess.Popen("nm-connection-editor")) 
 
         self.children = CenterBox(
             start_children=[self.power_button, self.date_time],
             center_children=[self.workspaces],
-            end_children=[self.active_window],
+            end_children=[self.active_window, self.bluetooth, self.network],
         )   
 
     def run_power_menu_script(self, *args):
@@ -62,6 +66,16 @@ class Bar(Window):
           "$HOME/.config/hyprnosis/configs/waybar/scripts/power_menu.sh"
         )
         subprocess.Popen([script_path])
+
+    def run_bluetooth_script(self, *args):
+        script_path = os.path.expandvars(
+            "$HOME/.config/hyprnosis/modules/bluetooth.sh"
+        )
+        subprocess.Popen([script_path]) 
+       
+    #def run_scripts(self. *args):
+        #script_path = $script_path
+        #subprocess.Popen([script_path])
 
 if __name__ == "__main__":
     bar = Bar()
@@ -75,6 +89,6 @@ if __name__ == "__main__":
 
     style_monitor = monitor_file(get_relative_path("./styles/bar.css"))
     style_monitor.connect("changed", apply_stylesheet)
-    apply_stylesheet() # initial styling
+    apply_stylesheet()
 
     app.run()
