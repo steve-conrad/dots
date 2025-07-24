d0293308bb73030545b49e9fd712530934a5d9f6
steve-conrad <steve.conrad14@gmail.com>
2025-07-23 21:04:22 -0400

    modularized the bar and moved all of the buttons out of the main bar file into their own individual files

--- fabric/bar.py
+++ fabric/bar.py
@@ -5,18 +5,18 @@ import gi
 gi.require_version("Gdk", "3.0")
 from gi.repository import Gdk
 from fabric import Application
-from fabric.widgets.datetime import DateTime
 from fabric.widgets.centerbox import CenterBox
 from fabric.widgets.button import Button
 from fabric.widgets.wayland import WaylandWindow as Window
 from fabric.utils import get_relative_path, monitor_file
-from fabric.hyprland.widgets import (
-        Language,
-        WorkspaceButton,
-        Workspaces,
-        get_hyprland_connection,
-        ActiveWindow,
-)
+from modules._datetime import datetime
+from modules._powerbutton import powerbutton
+from modules._bluetooth import bluetooth
+from modules._workspaces import workspaces
+from modules._activewindow import activewindow
+from modules._audio import audio
+from modules._network import network
+from modules._notifications import notifications
 
 class Bar(Window):
     def __init__(self, **kwargs):
@@ -27,75 +27,29 @@ class Bar(Window):
             exclusivity="auto",
             **kwargs
         )
-       
-        screen = Gdk.Screen.get_default()        
+
+        #Get display size 
+        screen = Gdk.Screen.get_default()
         monitor_num = screen.get_primary_monitor()
         geometry = screen.get_monitor_geometry(monitor_num)
         monitor_width_px = geometry.width
-
+        
+        #Set bar width based on monitor
         bar_width = int(monitor_width_px * 0.50)
         bar_height = 30
         self.set_size_request(bar_width, bar_height)
 
-        self.power_button = Button(
-            label=" ",
-            name="power-button"
-        )
-        self.power_button.connect("clicked", self.run_power_menu_script)
-
-        self.date_time = DateTime(
-            name="date-time"
-        )
-
-        self.workspaces = Workspaces(
-            name="workspaces"
-        )
-
-        self.active_window = ActiveWindow(
-            name="active-window"
-        )
-
-        self.audio = Button(
-            label=" ",
-            name="audio-button"
-        )
-        self.audio.connect("clicked", lambda *args: subprocess.Popen("pavucontrol"))
-
-        self.bluetooth = Button(
-            label=" ",
-            name="bluetooth-button"
-        )
-        self.bluetooth.connect("clicked", self.run_bluetooth_script)
-
-        self.network = Button(
-            label=" ",
-            name="network-button"
-        )
-        self.network.connect("clicked", lambda *args: subprocess.Popen("nm-connection-editor"))
-
-        self.notifications = Button(
-            label=" ",
-            name="notifications-button"
-        )
-        self.notifications.connect("clicked", lambda *args: subprocess.Popen(["swaync-client", "-t", "-sw"]))
+        #Check if monitors changed or disconnect
+        screen.connect("monitors-changed", self.on_monitors_changed)
 
         self.children = CenterBox(
-            start_children=[self.power_button, self.active_window],
-            center_children=[self.workspaces],
-            end_children=[self.date_time, self.audio, self.bluetooth, self.network, self.notifications],
+            start_children=[powerbutton, activewindow],
+            center_children=[workspaces],
+            end_children=[datetime, audio, bluetooth, network, notifications],
         )   
-
-    def run_power_menu_script(self, *args):
-        script_path = os.path.expandvars(
-          "$HOME/.config/hyprnosis/configs/waybar/scripts/power_menu.sh"
-        )
-        subprocess.Popen([script_path])
-
-    def run_bluetooth_script(self, *args):
-        script_path = os.path.expandvars(
-            "$HOME/.config/hyprnosis/modules/bluetooth.sh"
-        )
-        subprocess.Popen([script_path]) 
+    
+    def on_monitors_changed(self, screen):
+        self.show_all()
        
 
 if __name__ == "__main__":
